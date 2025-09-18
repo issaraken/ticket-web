0191438ebc7286b211595f84cc0e42137c7f9fb5
issara-ken <kennoung@gmail.com>
2025-09-18 16:16:34 +0700

    add(): MUI data-grid

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,18 @@
 'use client'
 import Button from '@mui/material/Button'
 import { useRouter } from 'next/navigation'
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid'
+
+const rows: GridRowsProp = [
+  { id: 1, name: 'Data Grid', description: 'the Community version' },
+  { id: 2, name: 'Data Grid Pro', description: 'the Pro version' },
+  { id: 3, name: 'Data Grid Premium', description: 'the Premium version' },
+]
+
+const columns: GridColDef[] = [
+  { field: 'name', headerName: 'Product Name', width: 200 },
+  { field: 'description', headerName: 'Description', width: 300 },
+]
 
 const TicketsPage = () => {
   const router = useRouter()
@@ -10,11 +22,15 @@ const TicketsPage = () => {
       <div className="flex items-center justify-between">
         <span className="text-3xl font-semibold">Tickets</span>
         <Button
-          variant="outlined"
+          variant="contained"
           onClick={() => router.push('/tickets/create')}
         >
           เพิ่ม ticket
         </Button>
+      </div>
+
+      <div style={{ height: 300, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} />
       </div>
     </div>
   )
