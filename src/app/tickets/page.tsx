3ba86c70fe4b6654adbd357dc7cc2b7f2a9bc995
issara-ken <kennoung@gmail.com>
2025-09-18 15:58:37 +0700

    refactor(): ลบ shadcn-ui ออก น่าจะใช้เวลานาน

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,27 +1,10 @@
-import { Button } from '@components/ui/button'
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-
 const TicketsPage = () => {
   return (
-    <div className="bg-gray-50 min-h-screen p-6">
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle className="text-3xl font-semibold">Tickets</CardTitle>
-          <CardAction>
-            <Button variant="default" className="cursor-pointer">
-              เพิ่ม ticket
-            </Button>
-          </CardAction>
-        </CardHeader>
-
-        <CardContent></CardContent>
-      </Card>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <span className="text-3xl font-semibold">Tickets</span>
+        <button className="cursor-pointer">เพิ่ม ticket</button>
+      </div>
     </div>
   )
 }
